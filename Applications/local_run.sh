aaad611cf7e54dcc7ea49fa96f4173b653f65f15
startxfr <clarue@startx.fr>
2014-12-06 03:10:51 +0100

    correcting potential tcp/ip port confict when creating fedora 21 based's images

--- Applications/local_run.sh
+++ Applications/local_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -d -p 85:80 --name="fc21-lbpma" app-pma:fc21
-docker run -d -p 87:80 --name="fc21-lbrockmongo" app-rockmongo:fc21
+docker run -d -p 105:80 --name="fc21-lbpma" app-pma:fc21
+docker run -d -p 107:80 --name="fc21-lbrockmongo" app-rockmongo:fc21
--- Services/local_run.sh
+++ Services/local_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker run -d -p 81:80 --name="fc21-lbapache" sv-apache:fc21
-docker run -d -p 3307:3306 --name="fc21-lbmariadb" sv-mariadb:fc21
-docker run -d -p 11212:11211 --name="fc21-lbmemcache" sv-memcache:fc21
-docker run -d -p 27018:27017 --name="fc21-lbmongo" sv-mongo:fc21
-docker run -d -p 8001:8000 --name="fc21-lbnodejs" sv-nodejs:fc21
-docker run -d -p 83:80 --name="fc21-lbphp" sv-php:fc21
-docker run -d -p 5433:5432 --name="fc21-lbpostgres" sv-postgres:fc21
-docker run -d -p 22023:22 --name="fc21-lbssh" sv-ssh:fc21
+docker run -d -p 101:80 --name="fc21-lbapache" sv-apache:fc21
+docker run -d -p 3327:3306 --name="fc21-lbmariadb" sv-mariadb:fc21
+docker run -d -p 11232:11211 --name="fc21-lbmemcache" sv-memcache:fc21
+docker run -d -p 27038:27017 --name="fc21-lbmongo" sv-mongo:fc21
+docker run -d -p 8021:8000 --name="fc21-lbnodejs" sv-nodejs:fc21
+docker run -d -p 103:80 --name="fc21-lbphp" sv-php:fc21
+docker run -d -p 5453:5432 --name="fc21-lbpostgres" sv-postgres:fc21
+docker run -d -p 22043:22 --name="fc21-lbssh" sv-ssh:fc21
