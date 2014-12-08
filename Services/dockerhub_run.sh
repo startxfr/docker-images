efe59d18eb97d3e8bef5c263e6841ed9d926179b
startxfr <clarue@startx.fr>
2014-12-08 02:58:26 +0100

    change in auto-generation scripts

--- Services/dockerhub_run.sh
+++ Services/dockerhub_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-docker run -d -p 80:80 --name="apache" startx/sv-apache:centos7
-docker run -d -p 3306:3306 --name="mariadb" startx/sv-mariadb:centos7
-docker run -d -p 11211:11211 --name="memcache" startx/sv-memcache:centos7
-docker run -d -p 27017:27017 --name="mongo" startx/sv-mongo:centos7
-docker run -d -p 8000:8000 --name="nodejs" startx/sv-nodejs:centos7
-docker run -d -p 82:80 --name="php" startx/sv-php:centos7
-docker run -d -p 5432:5432 --name="postgres" startx/sv-postgres:centos7
-docker run -d -p 2002:2002 --name="ooconv" startx/sv-ooconv:centos7
-docker run -d -p 22022:22 --name="ssh" startx/sv-ssh:centos7
+docker run -d -p 80:80 --name="centos7-apache" startx/sv-apache:centos7
+docker run -d -p 3306:3306 --name="centos7-mariadb" startx/sv-mariadb:centos7
+docker run -d -p 11211:11211 --name="centos7-memcache" startx/sv-memcache:centos7
+docker run -d -p 27017:27017 --name="centos7-mongo" startx/sv-mongo:centos7
+docker run -d -p 8000:8000 --name="centos7-nodejs" startx/sv-nodejs:centos7
+docker run -d -p 82:80 --name="centos7-php" startx/sv-php:centos7
+docker run -d -p 5432:5432 --name="centos7-postgres" startx/sv-postgres:centos7
+docker run -d -p 2002:2002 --name="centos7-ooconv" startx/sv-ooconv:centos7
+docker run -d -p 22022:22 --name="centos7-ssh" startx/sv-ssh:centos7
