572c7a94dff0f4a87e98b71891cd9abcd4cabe50
startxfr <clarue@startx.fr>
2015-11-28 07:41:33 +0100

    adding travis

--- run-tests.sh
+++ run-tests.sh
@@ -3,11 +3,10 @@ set -ev
 mkdir /tmp/startx-docker-images;
 cd /tmp/startx-docker-images;
 git clone https://github.com/startxfr/docker-images.git /tmp/startx-docker-images
-sudo docker build -t fedora OS/
-sudo docker run -it --name="fedora" fedora bash
+sudo docker-compose build
 sudo docker-compose up -d
 sudo docker-compose logs
-exit 1;
+echo $TRAVIS_PULL_REQUEST
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	exit 0;
 fi
