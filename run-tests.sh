392948b0614b0259bfa3a6905d1412d70ba0309a
startxfr <clarue@startx.fr>
2015-12-05 19:54:39 +0100

    addapt test script to branch fc22

--- run-tests.sh
+++ run-tests.sh
@@ -5,13 +5,14 @@ set -ev
 mkdir /tmp/startx-docker-images;
 cd /tmp/startx-docker-images;
 git clone https://github.com/startxfr/docker-images.git .
-echo "========> TESTING OS Container (latest)"
+git checkout fc22
+echo "========> TESTING OS Container (fedora 22)"
 sudo docker-compose -f docker-compose-os.yml build
 sudo docker-compose -f docker-compose-os.yml up -d
-echo "========> TESTING SERVICES Containers (latest)"
+echo "========> TESTING SERVICES Containers (fedora 22)"
 sudo docker-compose -f docker-compose-sv.yml build
 sudo docker-compose -f docker-compose-sv.yml up -d
-#echo "========> TESTING APPLICATIONS Containers (latest)"
+#echo "========> TESTING APPLICATIONS Containers (fedora 22)"
 #sudo docker-compose -f docker-compose-app.yml build
 #sudo docker-compose -f docker-compose-app.yml up -d
 echo "=================> TEST ENDED SUCCESSFULLY"
