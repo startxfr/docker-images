65af668c63096335aac30f92342981627c629410
startxfr <clarue@startx.fr>
2016-06-28 01:57:04 +0200

    mise en place des test pour pma

--- run-tests.sh
+++ run-tests.sh
@@ -5,15 +5,18 @@ set -ev
 mkdir /tmp/startx-docker-images;
 cd /tmp/startx-docker-images;
 git clone https://github.com/startxfr/docker-images.git .
-echo "========> TESTING OS Container (latest)"
+echo "========> BUILDING OS Container (latest)"
 sudo docker-compose -f docker-compose-os.yml build
+echo "========> BUILDING SERVICES Containers (latest)"
+sudo docker-compose -f docker-compose-sv.yml build
+echo "========> BUILDING APPLICATIONS Containers (latest)"
+sudo docker-compose -f docker-compose-app.yml build
+echo "========> TESTING OS Container (latest)"
 sudo docker-compose -f docker-compose-os.yml up -d
 echo "========> TESTING SERVICES Containers (latest)"
-sudo docker-compose -f docker-compose-sv.yml build
 sudo docker-compose -f docker-compose-sv.yml up -d
-#echo "========> TESTING APPLICATIONS Containers (latest)"
-#sudo docker-compose -f docker-compose-app.yml build
-#sudo docker-compose -f docker-compose-app.yml up -d
+echo "========> TESTING APPLICATIONS Containers (latest)"
+sudo docker-compose -f docker-compose-app.yml up -d
 echo "=================> TEST ENDED SUCCESSFULLY"
 exit 0;
 
