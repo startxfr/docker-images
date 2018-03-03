55318617d13427482034f75a6fa831e46465edec
startxfr <clarue@startx.fr>
2018-03-03 02:28:55 +0100

    debug several containers

--- Services/nodejs/app.js
+++ Services/nodejs/app.js
@@ -1,8 +1,10 @@
+/* global process */
+
 // Load the http module to create an http server.
 var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+  response.end("Welcome to " + process.env.SX_SUMMARY + "\n");
 });
 server.listen(8080);
-console.log("Server is running");
+console.log("Server is running " + process.env.SX_ID + ":" + process.env.SX_VERSION);
