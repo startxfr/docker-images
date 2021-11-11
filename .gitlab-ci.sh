e35763e7a909fd3cfb0b93984a963733394ab12e
Christophe LARUE <dev@startx.fr>
2021-11-11 18:50:28 +0100

    [cicd] Improve pipeline for master

---
+++ .gitlab-ci.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# Display the debug context
+function DisplayContextDebug {
+    VS_PYTHON=$(python3 --version)
+    MDL_PYTHON=$(mdl --version)
+    SS_PYTHON=$(shellcheck --version)
+    DDSIZE=$(du -sh .)
+    if [[ $SX_DEBUG == true ]]; then
+    cat <<EOF
+    ======== DEBUG
+    python version     : $VS_PYTHON
+    mdl version        : $MDL_PYTHON
+    shellcheck version : $SS_PYTHON
+    directory size     : $DDSIZE
+    EOF
+    else
+    cat <<EOF
+    ======== DEBUG
+    python version     : $VS_PYTHON
+    directory size     : $DDSIZE
+    EOF
+    fi
+}
+
+
+
+
